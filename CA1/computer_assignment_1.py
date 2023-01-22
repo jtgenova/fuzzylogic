ed81805022d22f31689d1969e1c35da8786665b3
Joshua <jtgenova@cougarnet.uh.edu>
2023-01-22 13:57:45 -0600

    pylint score of 10

--- CA1/computer_assignment_1.py
+++ CA1/computer_assignment_1.py
@@ -1,29 +1,29 @@
-# -------------------------------------------------------------------------------
+#  -------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 # Genova
 # Joshua
 # Computer Assignment: 1
 # -------------------------------------------------------------------------------
-# Description: Using python 3.8, calculate the first nine integers of the Fibonacci sequence.
+"""Description: Using python 3.8, calculate the first nine integers of the Fibonacci sequence."""
 # START RUN
 # -------------------------------------------------------------------------------
+
 import time
 start_time = time.time()
 
-n1 = 0
-n2 = 1
-num_seq = 9
-for i in range(num_seq):
-    n_current = n1 + n2
+N1 = 0
+N2 = 1
+NUMSEQ = 9
+for i in range(NUMSEQ):
+    NCURRENT = N1 + N2
     if i == 0:
-        n_current = 0
+        NCURRENT = 0
     if i == 1:
-        n_current = 1
-    print(f"Sequence {i+1} is {n_current}")
-    n1 = n2
-    n2 = n_current
-
-print("Process finished --- %s seconds ---" % (time.time() - start_time))
+        NCURRENT = 1
+    print(f"Sequence {i+1} is {NCURRENT}")
+    N1 = N2
+    N2 = NCURRENT
+print(f"Process finished --- {time.time() - start_time} seconds ---")
 # -------------------------------------------------------------------------------
 # Run time: ~0.003 seconds
 # END RUN
