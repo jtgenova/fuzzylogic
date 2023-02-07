978fb70dd3ba6353db7f010c235773f350d04f12
Joshua <jtgenova@cougarnet.uh.edu>
2023-02-07 15:44:52 -0600

    Fixed formatting Fixed list

--- Assignments/ece6397_genova_ca_3.py
+++ Assignments/ece6397_genova_ca_3.py
@@ -3,46 +3,55 @@ ECE 6397: Fuzzy Logic
 Professor: Dr. Steven Provence
 Author: Joshua Genova
 Computer Assignment #3
-Description: Given a plain text file that contains the name of a set in the first line and the members of the set in the following lines, calculate the Power Set of the set.
-             Display the results in the body of the output --the empty set can be either Ø or set(). After all of the members of the power set are displayed, print the number of the members of the Power Set.
-             The script should be capable of reading in any text file of the same format and calculting a given Power Set.
+Description: Given a plain text file that contains the name
+             of a set in the first line and the members of
+             the set in the following lines, calculate the Power Set of the set.
+             Display the results in the body of the output
+             --the empty set can be either Ø or set().
+             After all of the members of the power set are displayed,
+             print the number of the members of the Power Set.
+             The script should be capable of reading in any text file
+             of the same format and calculting a given Power Set.
 Deadline: February 10, 2023 11:59PM
 """
 import time
-import ece6397_genova_helper_headerfooter as header_footer
-import os
 import sys
 from itertools import chain, combinations
+from ast import literal_eval
+import ece6397_genova_helper_headerfooter as header_footer
 
 
 def power_set(text_file):
-    with open(text_file) as f:
-        lines = f.readlines()
+    """
+    Create power set list from given set list from a text file.
+    """
+    with open(text_file, encoding='utf-8') as input_file:
+        lines = input_file.readlines()
 
-    new_list = list(lines[1:len(lines)])
-    print(f'Set {lines[0]}')
-    total_members = len(new_list)
-    res = []
-    for sub in new_list:
-        res.append(sub.replace("\n", ""))
-
-    while("" in res):
-        res.remove("")
-    total_members = len(res)
-    new_set = list(chain.from_iterable(combinations(res, r) for r in range(len(res)+1)))
-    for i in range(len(new_set)):
-        print(new_set[i])
-    print(f'Number of members of Power Set: {2**total_members}')
-    print(f'Number of members of Power Set: {len(new_set)}')
+    # remove \n
+    rm_new_line = []
+    for sub in lines:
+        rm_new_line.append(sub.replace("\n", ""))
+    # remove empty
+    while "" in rm_new_line:
+        rm_new_line.remove("")
+    # create new set
+    new_set = list(rm_new_line[1:len(rm_new_line)])
+    new_set = [literal_eval(i) for i in new_set]
+    print(f'Set {rm_new_line[0]}: {new_set}')
+    # print(f'Number of members of Power Set: {2**len(set)}')
+    return list(chain.from_iterable(combinations(new_set, r) for r in range(len(new_set)+1)))
 ##################################################################################################
 if __name__=="__main__":
     CA_NUM = 3
-    DESCRIPTION = "Given a plain text file that contains the name of a set in the first line and the members of the set in the following lines, calculate the Power Set of the set."
+    DESCRIPTION = "Given a plain text file, calculate the Power Set of the set."
     header_footer.header(CA_NUM, DESCRIPTION)
-    ROOT = os.getcwd() + '\Assignments'
-    # filename = ROOT + '\Set_for_CA3.txt'
-    filename = sys.argv[1]
-    power_set(filename)
     start_time = time.time()
+    FILENAME = sys.argv[1]
+    list_power_set = power_set(FILENAME)
+    MAXIDX = len(list_power_set)
+    for i in range(MAXIDX):
+        print(list_power_set[i])
+    print(f'Number of members of Power Set: {len(list_power_set)}')
     final_time = time.time() - start_time
     header_footer.footer(final_time)
