e8e851e7672ae495d0d1ed9341c3f37502ad0634
Joshua <jtgenova@cougarnet.uh.edu>
2023-02-21 15:29:51 -0600

    pushing

--- Assignments/ece6397_genova_ca_5.py
+++ Assignments/ece6397_genova_ca_5.py
@@ -12,23 +12,22 @@ Deadline: February 26, 2023 11:59PM
 """
 import time
 import ece6397_genova_helper_headerfooter as header_footer
+import ece6397_genova_ca_4 as op
 import numpy as np
 from interval import interval
 
 def data_est(k, u_k, y_k):
-    u_int =  np.zeros(shape=(8,1))
-    y_int =  np.zeros(shape=(8,1))
-    print(interval(u_k[0]))
-    # change into intervals
-    for i in range(8):
-        u_int[i] = interval(u_k[i])
-        y_int[i] = interval(y_k[i])
+
         
-    rows = 8
-    A_mat = np.array([-y_k[0], 0, u_k[0], 0])
-    # A_mat = np.zeros(shape=(rows,4))
-    # A_mat[0] = [-y_k[0], 0, u_k[0], 0]
-    print(A_mat)
+    rows = len(k)
+    
+    u_k_interval = [[] for i in range(rows)]
+    y_k_interval = [[] for i in range(rows)]
+
+    for i in range(rows):
+        u_k_interval[i] = interval(u_k[i])
+        y_k_interval[i] = interval(y_k[i])
+    print(u_k_interval)
     # idx = 0
     # for i in range(0, rows, 2):
     #     l_mat[i] = xf[idx]
@@ -43,8 +42,8 @@ if __name__=="__main__":
     header_footer.header(CA_NUM, DESCRIPTION)
     start_time = time.time()
     k = list(range(8))
-    u_k = np.array([[1,3], [2,3], [3,4], [4,6], [4,5], [4,4.5], [5,5.5], [5,6]])
-    y_k = np.array([[1,2], [2,3], [2,3.5], [2.25,4], [2,3.5], [1.25,2], [1.5,2], [2,2.25]])
+    u_k = [[1,3], [2,3], [3,4], [4,6], [4,5], [4,4.5], [5,5.5], [5,6]]
+    y_k = [[1,2], [2,3], [2,3.5], [2.25,4], [2,3.5], [1.25,2], [1.5,2], [2,2.25]]
     data_est(k, u_k, y_k)
     final_time = time.time() - start_time
     header_footer.footer(final_time)
