9407631cfab57bb7164a242f106905483c6dff34
jtgenova <jtgenova@cougarnet.uh.edu>
2023-02-27 23:45:11 -0600

    pylint 10/10 graph good, questionable if plt.show() is okay at end u(c) should be an array or piecewise?

--- Assignments/ece6397_genova_ca_6.py
+++ Assignments/ece6397_genova_ca_6.py
@@ -22,34 +22,28 @@ Description:
 Deadline: March 5, 2023 11:59PM
 """
 import time
-from interval import interval
 from skfuzzy.fuzzymath import fuzzy_add, fuzzy_sub, fuzzy_mult
 import numpy as np
 from matplotlib import pyplot as plt
 import ece6397_genova_helper_headerfooter as header_footer
 
-def calc_vals(set_range, start_zero, mid_one, end_zero):
-    slope1 = 1/(mid_one - start_zero)
-    b1 = -slope1*start_zero
-    slope2 = -1/(end_zero - mid_one)
-    b2 = -slope2*end_zero
-    # print(f'slope1 = {slope1}, b1 = {b1}')
-    # print(f'slope2 = {slope2}, b2 = {b2}')
-
-def add_fuzzy(a, mf_a, b, mf_b):
-    c_add, mf_cadd = fuzzy_add(a, mf_a, b, mf_b)
+def add_fuzzy(a_set, mf_a, b_set, mf_b):
+    """
+    use fuzzy_add to add two sets and print the results.
+    """
+    c_add, mf_cadd = fuzzy_add(a_set, mf_a, b_set, mf_b)
 
     plt.figure(figsize=(15,5))
     plt.subplot(1,3,1)
     plt.xlim([0, 20])
-    plt.plot(a, mf_a)
+    plt.plot(a_set, mf_a)
     plt.xlabel("a", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{A}(a)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("Set A", fontdict ={'family':'serif','color':'blue','size':12})
 
     plt.subplot(1,3,2)
     plt.xlim([0, 20])
-    plt.plot(b, mf_b)
+    plt.plot(b_set, mf_b)
     plt.xlabel("b", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{B}(b)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("Set B", fontdict ={'family':'serif','color':'blue','size':12})
@@ -60,23 +54,25 @@ def add_fuzzy(a, mf_a, b, mf_b):
     plt.xlabel("c", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{C}(c)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("A+B=C", fontdict ={'family':'serif','color':'blue','size':12})
-    
-    # plt.show(block=False)
+    return c_add, mf_cadd
 
-def subtract_fuzzy(a, mf_a, b, mf_b):
-    c_sub, mf_csub = fuzzy_sub(a, mf_a, b, mf_b)
+def subtract_fuzzy(a_set, mf_a, b_set, mf_b):
+    """
+    use fuzzy_sub to subtract sets and print the results.
+    """
+    c_sub, mf_csub = fuzzy_sub(a_set, mf_a, b_set, mf_b)
 
     plt.figure(figsize=(15,5))
     plt.subplot(1,3,1)
     plt.xlim([-5, 13])
-    plt.plot(a, mf_a)
+    plt.plot(a_set, mf_a)
     plt.xlabel("a", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{A}(a)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("Set A", fontdict ={'family':'serif','color':'blue','size':12})
 
     plt.subplot(1,3,2)
     plt.xlim([-5, 13])
-    plt.plot(b, mf_b)
+    plt.plot(b_set, mf_b)
     plt.xlabel("b", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{B}(b)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("Set B", fontdict ={'family':'serif','color':'blue','size':12})
@@ -87,23 +83,25 @@ def subtract_fuzzy(a, mf_a, b, mf_b):
     plt.xlabel("c", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{C}(c)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("A-B=C", fontdict ={'family':'serif','color':'blue','size':12})
-    
-    # plt.show(block=False)
+    return c_sub, mf_csub
 
-def multiply_fuzzy(a, mf_a, b, mf_b):
-    c_mult, mf_cmult = fuzzy_mult(a, mf_a, b, mf_b)
+def multiply_fuzzy(a_set, mf_a, b_set, mf_b):
+    """
+    use fuzzy_mult to multiply sets and print the results.
+    """
+    c_mult, mf_cmult = fuzzy_mult(a_set, mf_a, b_set, mf_b)
 
     plt.figure(figsize=(15,5))
     plt.subplot(1,3,1)
     plt.xlim([0, 65])
-    plt.plot(a, mf_a)
+    plt.plot(a_set, mf_a)
     plt.xlabel("a", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{A}(a)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("Set A", fontdict ={'family':'serif','color':'blue','size':12})
 
     plt.subplot(1,3,2)
     plt.xlim([0, 65])
-    plt.plot(b, mf_b)
+    plt.plot(b_set, mf_b)
     plt.xlabel("b", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{B}(b)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("Set B", fontdict ={'family':'serif','color':'blue','size':12})
@@ -114,10 +112,7 @@ def multiply_fuzzy(a, mf_a, b, mf_b):
     plt.xlabel("c", fontdict={'family':'serif','color':'darkred','size':10})
     plt.ylabel(r'$u_{C}(c)$', fontdict={'family':'serif','color':'darkred','size':10})
     plt.title("A*B=C", fontdict ={'family':'serif','color':'blue','size':12})
-    
-    # plt.show(block=False)
-
-
+    return c_mult, mf_cmult
 ##################################################################################################
 if __name__=="__main__":
     CA_NUM = 6
@@ -128,10 +123,25 @@ if __name__=="__main__":
     a_vals = np.r_[0, 0, 0, 0, 0.5, 1, 0.8, 0.6, 0.4, 0.2, 0, 0]
     range_b = np.r_[2:5+1]
     b_vals = np.r_[0, 0, 1, 0]
-    calc_vals(range_a, 4, 6, 11)
-    add_fuzzy(range_a, a_vals, range_b, b_vals)
-    subtract_fuzzy(range_a, a_vals, range_b, b_vals)
-    multiply_fuzzy(range_a, a_vals, range_b, b_vals)
+    print('Given:')
+    print(f'Set A = {range_a}')
+    print(f'Set B = {range_b}\n')
+    sum_c, sum_mfc = add_fuzzy(range_a, a_vals, range_b, b_vals)
+    print('1. A + B = C')
+    print(f'C = {sum_c}')
+    print(f'\u03BC(c) = {sum_mfc}\n')
+
+    diff_c, diff_mfc = subtract_fuzzy(range_a, a_vals, range_b, b_vals)
+    print('2. A - B = C')
+    print(f'C = {diff_c}')
+    print(f'\u03BC(c) = {diff_mfc}\n')
+
+    prod_c, prod_mfc = multiply_fuzzy(range_a, a_vals, range_b, b_vals)
+    print('3. A * B = C')
+    print(f'C = {prod_c}')
+    print(f'\u03BC(c) = {prod_mfc}')
+    plt.show(block=False)
     final_time = time.time() - start_time
     header_footer.footer(final_time)
     plt.show()
+    
